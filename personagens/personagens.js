f78600fbdd221095898b9f379ba4043eb816ab2d
vaniamesquita <ninha.mesquita@gmail.com>
2022-01-25 10:18:10 -0300

    :alien: pagina personagens reformulada

--- personagens/personagens.js
+++ personagens/personagens.js
@@ -1,34 +1,11 @@
-//contador
-const personagensContador = document.getElementById("personagens");
-const luasContador = document.getElementById("luas");
-const planetasContador = document.getElementById("planetas");
-const navesContador = document.getElementById("naves");
+
 
 function swapiGet(param) {
   return axios.get(`https://swapi.dev/api/${param}`);
 }
 
-preencherContadores();
+
 preencherTabelaPersonagens();
-
-
-//promises: varias requisiçoes ao mesmo tempo, esperar todas para mostrar o resultado
-function preencherContadores() {
-  // personagensContador.innerHTML = swapiGet('people/')
-  // console.log("primeiro")
-  Promise.all([
-    swapiGet("people/"),
-    swapiGet("starships/"),
-    swapiGet("planets/"),
-    swapiGet("vehicles/"),
-  ]).then((results) => {
-    console.log(results);
-    personagensContador.innerHTML = results[0].data.count;
-    luasContador.innerHTML = results[1].data.count;
-    planetasContador.innerHTML = results[2].data.count;
-    navesContador.innerHTML = results[3].data.count;
-  });
-}
 
 
 //async await: so uma requisiçao - PERSONAGENS
